c82a8f0ce884d3259b8dea97df62888e635e1215
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-05-04 18:49:08 -0700

    refactor Experience output render

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -20,6 +20,21 @@ export function Experience({ data, setData, mode = "input" }) {
       data.length > 1 ? data.toSpliced(data.length - 1, 1) : data
     );
   };
+
+  // Filter job items into sections for output display
+
+  if (mode === "output") {
+    data.map((obj, indx) => {
+      const jobCompany = obj.filter(
+        (item) => item.name === "company" || item.name === "position"
+      );
+      const jobDate = obj.filter((item) =>
+        ["startDate", "endDate", "location"].includes(item.name)
+      );
+      const jobDescription = obj.filter((item) => item.name === "description");
+    });
+  }
+
   return (
     <>
       {/* RENDER INPUT */}
@@ -63,7 +78,7 @@ export function Experience({ data, setData, mode = "input" }) {
       )}
 
       {/* RENDER OUTPUT */}
-      {mode === "output" &&
+      {/* {mode === "output" &&
         data.map((obj, index) => (
           <div className="schoolOutput" key={index}>
             <div className="schoolName">
@@ -93,7 +108,62 @@ export function Experience({ data, setData, mode = "input" }) {
               ))}
             </div>
           </div>
-        ))}
+        ))} */}
+
+      {/* ////////////////////////////////////////////// */}
+      {/* ////////////////////////////////////////////// */}
+      {/* ////////////////////////////////////////////// */}
+      {/* REFACTOR RENDER OUTPUT */}
+      {mode === "output" &&
+        data.map((obj, indx) => {
+          const jobCompany = obj.filter(
+            (item) => item.name === "company" || item.name === "position"
+          );
+          const jobDate = obj.filter(
+            (item) => item.name === "startDate" || item.name === "endDate"
+          );
+          const jobLoc = obj.filter((item) => item.name === "location");
+          const jobDescription = obj.filter(
+            (item) => item.name === "description"
+          );
+          return (
+            <div className="jobOutput" key="index">
+              <div className="jobTopPart">
+                <div className="jobCompany">
+                  {jobCompany.map((each, i) =>
+                    each.name === "company" ? (
+                      <h4>{each.val}</h4>
+                    ) : (
+                      <p>{each.val}</p>
+                    )
+                  )}
+                </div>
+                <div className="jobDateLoc">
+                  <div className="jobDate">
+                    {jobDate.map((each, i) =>
+                      each.name === "startDate" ? (
+                        <p>{each.val}</p>
+                      ) : (
+                        <p>&nbsp;- {each.val} </p>
+                      )
+                    )}
+                  </div>
+                  <div className="jobLoc">
+                    {jobLoc.map((each, i) => (
+                      <p>{each.val}</p>
+                    ))}
+                  </div>
+                </div>
+              </div>
+
+              <div className="jobBottomPart">
+                {jobDescription.map((each, i) => (
+                  <div className="jobDescription">{each.val}</div>
+                ))}
+              </div>
+            </div>
+          );
+        })}
     </>
   );
 }
